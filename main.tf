d8d0b041244d4f5f7783e1ba5026b73aa7d53000
Severiano Badajoz <me@seve.dev>
2024-06-18 23:53:22 +0000

    fix mod list and move to own file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     fly = {
-      source = "andrewbaxter/fly"
+      source  = "andrewbaxter/fly"
       version = "0.1.13"
     }
   }
@@ -11,49 +11,49 @@ provider "fly" {}
 
 resource "fly_app" "minecraft" {
   name = "seve-minecraft"
-  org = "personal"
+  org  = "personal"
 }
 
 resource "fly_volume" "mc_volume" {
-  app = "seve-minecraft"
-  name = "mc_volume"
-  size = 15
+  app    = "seve-minecraft"
+  name   = "mc_volume"
+  size   = 15
   region = "sjc"
 
   depends_on = [fly_app.minecraft]
 }
 
 resource "fly_ip" "mcIP" {
-  app = "seve-minecraft"
+  app  = "seve-minecraft"
   type = "v4"
 
   depends_on = [fly_app.minecraft]
 }
 
 resource "fly_machine" "mcServer" {
-  name = "mc-server"
+  name   = "mc-server"
   region = "sjc"
-  app = "seve-minecraft"
+  app    = "seve-minecraft"
   image  = "itzg/minecraft-server:latest"
 
   env = {
-    EULA                    = "TRUE"
-    ENABLE_AUTOSTOP         = "TRUE"
-    AUTOSTOP_TIMEOUT_EST    = 120
-    AUTOSTOP_TIMEOUT_INIT   = 120
-    MEMORY                  = "7G"
-    AUTOSTOP_PKILL_USE_SUDO = "TRUE"
-    TYPE                    = "MODRINTH"
-    MODRINTH_MODPACK        = "https://modrinth.com/modpack/sop"
-    MODRINTH_PROJECTS       = "plasmo-voice, double-doors, villager-names-serilum, universal-graves, styled-nicknames"
+    EULA                           = "TRUE"
+    ENABLE_AUTOSTOP                = "TRUE"
+    AUTOSTOP_TIMEOUT_EST           = 120
+    AUTOSTOP_TIMEOUT_INIT          = 120
+    MEMORY                         = "7G"
+    AUTOSTOP_PKILL_USE_SUDO        = "TRUE"
+    TYPE                           = "MODRINTH"
+    MODRINTH_MODPACK               = "https://modrinth.com/modpack/sop"
+    MODRINTH_PROJECTS              = file("${path.module}/mods.txt")
     MODRINTH_DOWNLOAD_DEPENDENCIES = "optional"
-    WHITELIST               = file("${path.module}/whitelist.txt")
+    WHITELIST                      = file("${path.module}/whitelist.txt")
     ENFORCE_WHITELIST              = "TRUE"
-    EXISTING_WHITELIST_FILE       = "MERGE"
-    OPS = "S33V"
+    EXISTING_WHITELIST_FILE        = "MERGE"
+    OPS                            = "S33V"
   }
 
- services = [
+  services = [
     {
       ports = [
         {
@@ -63,7 +63,7 @@ resource "fly_machine" "mcServer" {
       protocol      = "tcp"
       internal_port = 25565
     },
-        {
+    {
       ports = [
         {
           port = 25565
@@ -80,7 +80,7 @@ resource "fly_machine" "mcServer" {
     }
   ]
 
-  cpus     = 4
+  cpus   = 4
   memory = 8192
 
   depends_on = [fly_volume.mc_volume, fly_app.minecraft]
